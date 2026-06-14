0cc58fc3f39392539e95040303a9336d84912c86
flybyraj <669raj@gmail.com>
2026-06-14 14:00:00 +0530

    Study Progress: Staged Lec2/q5.py

---
+++ Lec2/q5.py
@@ -0,0 +1,8 @@
+## WAP to check if a number is a multiple of 7 or not.
+
+num = int(input("Enter the number : "))
+
+if num % 7 == 0:
+    print(num , "is multiple of 7")
+else :
+    print(num , "is not mltiple of 7")
